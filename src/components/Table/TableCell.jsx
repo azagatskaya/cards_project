57140de0faf1bc82a66a9b13dd37191a0a0cb331
Alya Zagatskaya <alya.zagacki@gmail.com>
2022-09-25 00:04:19 +0300

    fixed clear add field issue

--- src/components/Table/TableCell.jsx
+++ src/components/Table/TableCell.jsx
@@ -2,47 +2,52 @@ import React, {useEffect, useState} from 'react';
 import styles from './Table.module.scss';
 
 export default function TableCell({
-  cellPropName,
-  initialValue,
-  handleInputBlur,
-  isRowEditable,
-  isCanceled,
-}) {
-  const [newCellValue, setNewCellValue] = useState(initialValue);
+                                      cellPropName,
+                                      initialValue,
+                                      handleInputBlur,
+                                      isRowEditable,
+                                      isCanceled,
+                                  }) {
+    const [newCellValue, setNewCellValue] = useState(initialValue);
 
-  console.log('updated cell', newCellValue);
-  const handleInputChange = (e) => {
-    setNewCellValue(e.target.value);
-  };
+    useEffect(() => {
+        console.log('initialValue changed: setNewCellValue =', initialValue);
+        setNewCellValue(initialValue);
+    }, [initialValue, isCanceled]);
 
-  useEffect(() => {
-    console.log('isCanceled changed: setNewCellValue =', initialValue);
-    setNewCellValue(initialValue);
-  }, [isCanceled]);
+    console.log('updated cell', newCellValue);
+    const handleInputChange = (e) => {
+        setNewCellValue(e.target.value);
+    };
 
-  const handleBlur = (e) => {
-    handleInputBlur({[cellPropName]: e.target.value});
-  };
+    // useEffect(() => {
+    //     console.log('isCanceled changed: setNewCellValue =', initialValue);
+    //     setNewCellValue(initialValue);
+    // }, [isCanceled]);
 
-  const isCellEditable =
+    const handleBlur = (e) => {
+        handleInputBlur({[cellPropName]: e.target.value});
+    };
+
+    const isCellEditable =
         isRowEditable &&
         cellPropName !== 'id' &&
         cellPropName !== 'numberOfCards' &&
         cellPropName !== 'date';
 
-  return (
-    <td className={styles.table__cell}>
-      <input
-        disabled={!isCellEditable}
-        className={isCellEditable ?
+    return (
+        <td className={styles.table__cell}>
+            <input
+                disabled={!isCellEditable}
+                className={isCellEditable ?
                     `${styles.input} ${styles.input__editable}` :
                     styles.input
-        }
-        onChange={handleInputChange}
-        onBlur={handleBlur}
-        value={newCellValue}
-        type="text"
-      />
-    </td>
-  );
+                }
+                onChange={handleInputChange}
+                onBlur={handleBlur}
+                value={newCellValue}
+                type="text"
+            />
+        </td>
+    );
 }
--- src/components/Table/TableRow.jsx
+++ src/components/Table/TableRow.jsx
@@ -5,93 +5,93 @@ import TableCellActions from './TableCellActions.jsx';
 import TableButtonsOnAdd from './TableButtonsOnAdd';
 
 export default function TableRow({
-  row,
-  rowId,
-  handleDelete,
-  handleSaveChanges,
-  isEditable,
-  handleAddNewItem,
-}) {
-  const [initialCellValues, setInitialCellValues] = useState(row);
-  const [cellValues, setCellValues] = useState(row);
-  const [isRowEditable, setIsRowEditable] = useState(isEditable);
-  const [isCanceled, setIsCanceled] = useState(false);
+                                     row,
+                                     rowId,
+                                     handleDelete,
+                                     handleSaveChanges,
+                                     isEditable,
+                                     handleAddNewItem,
+                                 }) {
+    const [initialCellValues, setInitialCellValues] = useState(row);
+    const [cellValues, setCellValues] = useState(row);
+    const [isRowEditable, setIsRowEditable] = useState(isEditable);
+    const [isCanceled, setIsCanceled] = useState(false);
 
-  console.log('initialCellValues on start', initialCellValues);
+    console.log('initialCellValues on start', initialCellValues);
 
-  const handleEditClick = () => {
-    setIsRowEditable(true);
-    setIsCanceled(false);
-  };
-  const handleCancelClick = () => {
-    setIsRowEditable(false);
-    setIsCanceled(true);
-    setCellValues({...initialCellValues});
-  };
-  const handleClearAddField = () => {
-    console.log('handleClearAddField');
-    setIsCanceled(true);
-    setCellValues({...initialCellValues});
-  };
-  const handleSaveClick = () => {
-    setIsRowEditable((prevState) => !prevState);
-    handleSaveChanges(rowId, cellValues);
-    setInitialCellValues(cellValues);
-  };
+    const handleEditClick = () => {
+        setIsRowEditable(true);
+        setIsCanceled(false);
+    };
+    const handleCancelClick = () => {
+        setIsRowEditable(false);
+        setIsCanceled(true);
+        setCellValues({...initialCellValues});
+    };
+    const handleClearAddField = () => {
+        console.log('handleClearAddField');
+        setIsCanceled(true);
+        setCellValues({...initialCellValues});
+    };
+    const handleSaveClick = () => {
+        setIsRowEditable((prevState) => !prevState);
+        handleSaveChanges(rowId, cellValues);
+        setInitialCellValues(cellValues);
+    };
 
-  const handleInputBlur = (newValue) => {
-    console.log('input blur');
-    setCellValues((prevState) => {
-      return {...prevState, ...newValue};
-    });
-  };
+    const handleInputBlur = (newValue) => {
+        console.log('input blur');
+        setCellValues((prevState) => {
+            return {...prevState, ...newValue};
+        });
+    };
 
-  const handleAddClick = () => {
-    console.log('handleAddClick');
-    setIsCanceled(false);
-    handleAddNewItem(cellValues);
-    handleClearAddField();
-  };
+    const handleAddClick = () => {
+        console.log('handleAddClick');
+        // setIsCanceled(false);
+        handleAddNewItem(cellValues);
+        handleClearAddField();
+    };
 
-  const renderEditButtons = () => {
-    return (<TableCellActions
-      key={'row' + rowId.toString()}
-      rowId={rowId}
-      cellValue="actions"
-      handleEditClick={handleEditClick}
-      handleCancelClick={handleCancelClick}
-      handleSaveClick={handleSaveClick}
-      handleDelete={handleDelete}
-      isEditable={isRowEditable}
-    />);
-  };
-
-  const renderAddButtons = () => {
-    return (<TableButtonsOnAdd
-      key={'add'}
-      handleAddNewItem={handleAddClick}
-      handleClearAddField={handleClearAddField}
-    />);
-  };
-
-  const actionCell = rowId === 'add' ? renderAddButtons() : renderEditButtons();
-
-  return (
-    <tr className={styles.table__row}>
-      {Object.entries(cellValues).map(([key, value]) => {
-        return (
-          <TableCell
-            cellPropName={key}
-            key={`${rowId}_${key}`}
-            initialValue={value}
+    const renderEditButtons = () => {
+        return (<TableCellActions
+            key={'row' + rowId.toString()}
             rowId={rowId}
-            isRowEditable={isRowEditable}
-            isCanceled={isCanceled}
-            handleInputBlur={handleInputBlur}
-          />
-        );
-      })}
-      {actionCell}
-    </tr>
-  );
+            cellValue="actions"
+            handleEditClick={handleEditClick}
+            handleCancelClick={handleCancelClick}
+            handleSaveClick={handleSaveClick}
+            handleDelete={handleDelete}
+            isEditable={isRowEditable}
+        />);
+    };
+
+    const renderAddButtons = () => {
+        return (<TableButtonsOnAdd
+            key={'add'}
+            handleAddNewItem={handleAddClick}
+            handleClearAddField={handleClearAddField}
+        />);
+    };
+
+    const actionCell = rowId === 'add' ? renderAddButtons() : renderEditButtons();
+    return (
+        <tr className={styles.table__row}>
+            {Object.entries(cellValues).map(([key, value]) => {
+                console.log('initialValue', value);
+                return (
+                    <TableCell
+                        cellPropName={key}
+                        key={`${rowId}_${key}`}
+                        initialValue={value}
+                        rowId={rowId}
+                        isRowEditable={isRowEditable}
+                        isCanceled={isCanceled}
+                        handleInputBlur={handleInputBlur}
+                    />
+                );
+            })}
+            {actionCell}
+        </tr>
+    );
 }
