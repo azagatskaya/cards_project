aefa86408b048abb6d53676ce88f4e98a5100fee
Alya Zagatskaya <alya.zagacki@gmail.com>
2022-09-24 23:42:35 +0300

    fixed backface-visibility issue for chrome

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,66 +2,65 @@ import React from 'react';
 import styles from './Card.module.scss';
 
 function Card({
-  word,
-  transcription,
-  value,
-  onPrevClick,
-  onNextClick,
-  id,
-  cardsCount,
-}) {
-  const [isRotated, setIsRotated] = React.useState(false);
-  const handleRotateClick = () => {
-    setIsRotated((prevState) => !prevState);
-  };
+                  word,
+                  transcription,
+                  value,
+                  onPrevClick,
+                  onNextClick,
+                  id,
+                  cardsCount,
+              }) {
+    const [isRotated, setIsRotated] = React.useState(false);
+    const handleRotateClick = () => {
+        setIsRotated((prevState) => !prevState);
+    };
 
-  const handlePrevClick = (event) => {
-    event.stopPropagation();
-    onPrevClick();
-  };
-  const handleNextClick = (event) => {
-    event.stopPropagation();
-    onNextClick();
-  };
+    const handlePrevClick = (event) => {
+        event.stopPropagation();
+        onPrevClick();
+    };
+    const handleNextClick = (event) => {
+        event.stopPropagation();
+        onNextClick();
+    };
 
-  let cardClasses; let backClasses;
+    let cardClasses;
+    let backClasses;
 
-  if (isRotated) {
-    cardClasses = `${styles.card} ${styles.rotated}`;
-    backClasses = `${styles.card__back}  ${styles.back__rotated}`;
-  } else {
-    cardClasses = styles.card;
-    backClasses = `${styles.card__back}`;
-  }
-  return (
-    <div className={styles.card__wrapper}>
-      <div className={cardClasses} onClick={handleRotateClick}>
-        <button
-          className={styles.prev}
-          onClick={(event) => handlePrevClick(event)}
-        ></button>
-        <div>
-          {id + 1}/{cardsCount}
+    if (isRotated) {
+        cardClasses = `${styles.card} ${styles.rotated}`;
+        backClasses = `${styles.card__back}  ${styles.back__rotated}`;
+    } else {
+        cardClasses = styles.card;
+        backClasses = `${styles.card__back}`;
+    }
+    return (
+        <div className={styles.card__wrapper}>
+            <div className={cardClasses} onClick={handleRotateClick}>
+                <button
+                    className={styles.prev}
+                    onClick={(event) => handlePrevClick(event)}
+                ></button>
+                <div>{id + 1}/{cardsCount}</div>
+                <div className={styles.card__text}>
+                    <div className={styles.word}>{word}</div>
+                    <div className={styles.transcription}>{transcription}</div>
+                </div>
+                <button className={styles.button_rotate}></button>
+                <button
+                    className={styles.next}
+                    onClick={(event) => handleNextClick(event)}
+                ></button>
+            </div>
+            <div className={backClasses} onClick={handleRotateClick}>
+                <div>{id + 1}/{cardsCount}</div>
+                <div className={styles.card__text}>
+                    <div className={styles.value}>{value}</div>
+                </div>
+                <button className={styles.button_rotate}></button>
+            </div>
         </div>
-        <div className={styles.card__text}>
-          <div className={styles.word}>{word}</div>
-          <div className={styles.transcription}>{transcription}</div>
-        </div>
-        <button className={styles.button_rotate}></button>
-        <button
-          className={styles.next}
-          onClick={(event) => handleNextClick(event)}
-        ></button>
-      </div>
-      <div className={backClasses} onClick={handleRotateClick}>
-        <div>{id + 1}/{cardsCount}</div>
-        <div className={styles.card__text}>
-          <div className={styles.value}>{value}</div>
-        </div>
-        <button className={styles.button_rotate}></button>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Card;
